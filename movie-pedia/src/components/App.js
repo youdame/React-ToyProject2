bc0b3f0b153953e0650cae57af8ffd75f1e9a9fb
youdame <112458620+youdame@users.noreply.github.com>
2023-10-19 13:21:55 +0900

    Feat(movie-pedia) : mock 데이터 보여주는 화면 만들기

---
+++ movie-pedia/src/components/App.js
@@ -0,0 +1,6 @@
+import ReviewList from "./ReviewList";
+import items from "../mock.json";
+function App() {
+  return <div><ReviewList items = {items}/></div>
+}
+export default App;
---
+++ movie-pedia/src/index.js
@@ -0,0 +1,9 @@
+import ReactDOM from 'react-dom';
+import App from "./components/App";
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <App/>
+
+);
+
+
