dd8dbf8d0d407818569d9dbfb7e8b0f31780761e
youdame <112458620+youdame@users.noreply.github.com>
2023-10-19 16:28:36 +0900

    Feat(movie-pedia) : 최신순, 평점 순 선택해서 정렬하는 기능 추가

--- movie-pedia/src/components/App.js
+++ movie-pedia/src/components/App.js
@@ -1,6 +1,23 @@
 import ReviewList from "./ReviewList";
 import items from "../mock.json";
+import { useState } from "react";
 function App() {
-  return <div><ReviewList items = {items}/></div>
+
+  const [order, setOrder] = useState('createAt');
+  const sortedItems = items.sort((a, b) => b[order] - a[order]);
+
+  const handleNewestClick = () => { setOrder('createAt'); };
+
+  const handleBestClick = () => setOrder('rating');
+
+  return (
+
+    <div>
+      <div>
+        <button onClick={handleNewestClick}>최신순</button>
+        <button onClick={handleBestClick}>베스트순</button>
+      </div>
+      <ReviewList items={sortedItems} />
+    </div>);
 }
 export default App;
