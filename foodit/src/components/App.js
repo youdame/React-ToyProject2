80cef09a242a2f2a25155d8ea584cccc2e3b012d
youdame <112458620+youdame@users.noreply.github.com>
2023-10-20 00:36:23 +0900

    Feat(foodit) : 음식 지우기 기능 구현

--- foodit/src/components/App.js
+++ foodit/src/components/App.js
@@ -1,19 +1,25 @@
-import items from './mock.json';
+import mockItems from '../mock.json';
 import FoodList from './FoodList';
 import { useState } from 'react';
 
 
 function App() {
   const [order, setOrder] = useState('createdAt');
-  const sortedItems = items.sort((a, b) => b[order] - a[order]);
+  const [items, setItems] = useState(mockItems);
+
   const handleEarlyClick = () => setOrder('createdAt');
   const handleCalorieClick = () => setOrder('calorie');
-  
+  const handleDelete = (id) => {
+    const nextItems = items.filter((item) => item.id !== id);
+    setItems(nextItems);
+  };
+  const sortedItems = items.sort((a, b) => b[order] - a[order]);
+
   return (
     <div>
       <button onClick={handleEarlyClick}>최신순</button>
       <button onClick={handleCalorieClick}>칼로리순</button>
-      <FoodList items={sortedItems} />
+      <FoodList items={sortedItems} onDelete={handleDelete} />
     </div>
   );
 }
--- foodit/src/components/FoodList.js
+++ foodit/src/components/FoodList.js
@@ -5,10 +5,13 @@ function formatDate(value) {
   return `${date.getFullYear()}. ${date.getMonth() + 1}. ${date.getDate()}`;
 }
 
-function FoodListItem({ item }) {
+function FoodListItem({ item, onDelete }) {
 
   const { imgUrl, title, calorie, content, createdAt } = item;
 
+  const handleDeleteClick = () => {
+    onDelete(item.id);
+  }
   return (
     <div className="FoodListItem">
       <img src={imgUrl} alt={title} />
@@ -16,17 +19,17 @@ function FoodListItem({ item }) {
       <div>{calorie}</div>
       <div>{content}</div>
       <div>{formatDate(createdAt)}</div>
-
+      <button onClick={handleDeleteClick}>삭제</button>
     </div>
   );
 }
 
-function FoodList({ items }) {
+function FoodList({ items, onDelete }) {
   return (
     <ul className="FoodList">
       {items.map((item) => (
         <li>
-          <FoodListItem item={item} />
+          <FoodListItem item={item} onDelete={onDelete} />
         </li>
       ))}
     </ul>
