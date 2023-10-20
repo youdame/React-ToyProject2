9072a8966ce563414fe7222ec3ea111327fd8843
youdame <112458620+youdame@users.noreply.github.com>
2023-10-21 03:28:31 +0900

    Feat(foodit): 네트워크 로딩과 에러 처리

--- foodit/src/api.js
+++ foodit/src/api.js
@@ -2,7 +2,9 @@ export async function getFoods(order = 'createdAt', cursor = '', limit = 10){
   const query = `order=${order}&cursor=${cursor}&limit=${limit}`;
   const response = await fetch(`https://learn.codeit.kr/api/foods?${query}`);
 
-
+  if(!response.ok){
+    throw new Error('데이터를 불러오는데 실패했습니다');
+  }
   const result = await response.json();
   return result;
 }
--- foodit/src/components/App.js
+++ foodit/src/components/App.js
@@ -6,20 +6,34 @@ function App() {
   const [order, setOrder] = useState('createdAt');
   const [items, setItems] = useState([]);
   const [cursor, setCursor] = useState('');
-
+  const [isLoading, setIsLoading] = useState(false);
+  const [loadingError, setLoadingError] = useState(null);
 
 
   const handleEarlyClick = () => setOrder('createdAt');
   const handleCalorieClick = () => setOrder('calorie');
+
   const handleDelete = (id) => {
     const nextItems = items.filter((item) => item.id !== id);
     setItems(nextItems);
   };
+
   const sortedItems = items.sort((a, b) => b[order] - a[order]);
 
   const handleLoad = async (options) => {
-    const { foods, paging } = await getFoods(options);
+    let result;
+    try {
+      setIsLoading(true);
+      setLoadingError(null);
+      result = await getFoods(options);
 
+    } catch (error) {
+      setLoadingError(error);
+    } finally {
+      setIsLoading(false);
+    }
+
+    const { foods, paging } = result;
     setCursor(paging.nextCursor);
 
     if (!options.cursor) {
@@ -38,7 +52,8 @@ function App() {
       <button onClick={handleEarlyClick}>최신순</button>
       <button onClick={handleCalorieClick}>칼로리순</button>
       <FoodList items={sortedItems} onDelete={handleDelete} />
-      {cursor && <button onClick={handleLoadMore}>더 보기</button>}
+      {cursor && <button disabled={isLoading} onClick={handleLoadMore}>더 보기</button>}
+      {loadingError?.message && <p>loadingError.message</p>}
     </div>
   );
 }
