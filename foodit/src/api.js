5edb3f5f3cd937e730dfdddc1613dd2909e7c97b
youdame <112458620+youdame@users.noreply.github.com>
2023-10-21 09:42:15 +0900

    Fix : 두 프로젝트 api.js 오류 해결

--- foodit/src/api.js
+++ foodit/src/api.js
@@ -1,4 +1,4 @@
-export async function getFoods(order = 'createdAt', cursor = '', limit = 10){
+export async function getFoods({order = 'createdAt', cursor = '', limit = 10}){
   const query = `order=${order}&cursor=${cursor}&limit=${limit}`;
   const response = await fetch(`https://learn.codeit.kr/api/foods?${query}`);
 
--- movie-pedia/src/api.js
+++ movie-pedia/src/api.js
@@ -1,10 +1,14 @@
-export async function getReviews(order = 'createdAt') {
-  const query = `order=${order}`;
+export async function getReviews({
+  order = 'createdAt',
+  offset = 0,
+  limit = 6,
+}) {
+  const query = `order=${order}&offset=${offset}&limit=${limit}`;
   const response = await fetch(
-    `https://learn.codeit.kr/4062/film-reviews?${query}`
+    `https://learn.codeit.kr/api/film-reviews?${query}`
   );
   if (!response.ok) {
-    throw new Error('리뷰를 불러오는데 실패했습니다.');
+    throw new Error('리뷰를 불러오는데 실패했습니다');
   }
   const body = await response.json();
   return body;
