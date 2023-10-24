a7de6c33b00ab7d5fe13797928a8b231105d9018
youdame <112458620+youdame@users.noreply.github.com>
2023-10-24 15:48:54 +0900

    Feat : 이미지 파일 미리보기 기능 구현

--- movie-pedia/src/components/FileInput.js
+++ movie-pedia/src/components/FileInput.js
@@ -1,11 +1,38 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 function FileInput({ name, value, onChange }) {
+  const [preview, setPreview] = useState();
+
+  const inputRef = useRef();
   const handleChange = (e) => {
     const nextValue = e.target.files[0];
     onChange(name, nextValue);
   };
-  return <input type="file" onChange={handleChange} />;
+
+  const handleClearClick = () => {
+    const inputNode = inputRef.current;
+    if(!inputNode) return;
+    inputNode.value = "";
+    onChange(name, null);
+  }
+  useEffect(() => {
+    if(!value) return;
+    const nextPreview = URL.createObjectURL(value);
+    setPreview(nextPreview);
+
+    return () =>{
+      setPreview();
+      URL.revokeObjectURL(nextPreview);
+    }
+  }, [value]);   // 파일을 선택할 때마다 이미지 주소를 바꿈
+
+  return (
+    <div>
+      <img src = {preview} alt = "이미지 미리보기"/>
+      <input type="file" accept="image/png, image/jpeg" onChange={handleChange} ref={inputRef} />
+      {value && <button onClick={handleClearClick}>X</button>}
+    </div>
+  );
 }
 
 export default FileInput;
